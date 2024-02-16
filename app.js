d4660c2f0d505eb6f5ee618f87981c4fc4613cf6
Tadeo Sanchez <tsanchez0692@gmail.com>
2024-02-16 10:03:16 -0600

    mayus,simbolos

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ function funEncriptar(x1){
         }else{
             document.getElementById("advertencia").style.color= "black";
             document.getElementById("advertencia").style.fontSize = "20px";
-        var cadena = x1.replace("a", "bvc").replace("e", "test").replace("i", "rest").replace("o", "brda").replace("u","gest");
+        var cadena = x1.replace("a", "ai").replace("e", "enter").replace("i", "imes").replace("o", "ober").replace("u","ufat");
        
         document.getElementById("aDesencriptar").value= cadena;
         document.getElementById("aEncriptar").value= "";
@@ -36,11 +36,10 @@ function funEncriptar(x1){
 
 
 function funDesencriptar(x2){
-    var cadena = x2.replace("bvc", "a").replace("test", "e").replace("rest", "i").replace("brda", "o").replace("gest","u");
+    var cadena = x2.replace("ai", "a").replace("enter", "e").replace("imes", "i").replace("ober", "o").replace("ufat","u");
    
     document.getElementById("aDesencriptar").value= cadena;
     document.getElementById("aEncriptar").value= "";
-
 }
 
    
